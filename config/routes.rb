85ffca39dc80305a6839e047c58bd57ba86da573
Rafael Polo <rafael.polo@gmail.com>
2014-02-28 16:51:59 +0100

    route para sidekiq em dev e mais

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
+require 'sidekiq/web'
+
 Makingoff::Application.routes.draw do
+
+  mount Sidekiq::Web, at: "/sidekiq"
+
   resources :countries
   resources :genres
   resources :directors
   resources :movies
 
   get '/autocomplete.json', to: "movies#autocomplete"
+  get '/:urlized', to: "movies#show"
+  get '/list/:tipo/:id', to: "movies#list"
+  get '/list/cor/:cor', to: "movies#cor"
+  root 'movies#info'
 
-  root 'movies#index'
 end
