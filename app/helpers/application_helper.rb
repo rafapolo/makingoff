a20da6e6ea73a317af81ceb3518f725962cc9fb3
Rafael Polo <rafael.polo@gmail.com>
2014-02-28 16:49:50 +0100

    helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,10 +4,29 @@ module ApplicationHelper
     return "http://makingoff.org/?=#{m.mko_id}"
   end
 
-  def capa_for m
-    file = "app//assets/images/capas/#{m.id}.jpg"
-    path = File.exists?(Rails.root.join(file)) ? "capas/#{m.id}.jpg" : "capas/00.jpg"
-    image_tag path
+  def capa_image_for m
+    path = "http://graficautopica.net/assets/capas/#{m.id}.jpg"
+    image_tag(path)
+  end
+
+  def aprox_hours_ago(time)
+    aprox_hours = ((Time.now - time)/60/60).to_i
+    "#{aprox_hours} horas atrás" if aprox_hours > 1
+  end
+
+  def style_for m
+    case m.count
+      when nil
+        'border-preto'
+      when 0
+        'border-vermelho'
+      when 1..5
+        'border-laranja'
+      when 6..10
+        'border-amarelo'
+      else
+        'border-verde'
+      end
   end
 
 end
