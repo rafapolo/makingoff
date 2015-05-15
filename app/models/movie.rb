714fa0c4d8e5d6861fbc267e107e1b8b72a97d8c
Rafael Polo <rafael.polo@gmail.com>
2015-05-15 14:56:55 +0200

    Update movie.rb
    fix identation

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,7 @@
 class Movie < ActiveRecord::Base
-	has_and_belongs_to_many :directors
-	has_and_belongs_to_many :genres
-	has_and_belongs_to_many :countries
+  has_and_belongs_to_many :directors
+  has_and_belongs_to_many :genres
+  has_and_belongs_to_many :countries
   has_and_belongs_to_many :trackers
   has_many :seeds
 
